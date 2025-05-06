ef4d5dd3a1c733382ef2080aeb216ff51a7859af
elgaard99 <sean62359@edu.ucl.dk>
2025-05-06 14:44:36 +0200

    changes

--- SignalRv2/Data/Migrations/20250506122547_AddedMessage.Designer.cs
+++ SignalRv2/Data/Migrations/20250506122547_AddedMessage.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SignalRv2.Data;
 
@@ -11,9 +12,11 @@ using SignalRv2.Data;
 namespace SignalRv2.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250506122547_AddedMessage")]
+    partial class AddedMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SignalRv2/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using SignalRv2.Data;
+
+namespace SignalRv2.Models;
+
+public class Message
+{
+    int id;
+    ApplicationUser user;
+    string message;
+    DateTime timeSent;
+}
